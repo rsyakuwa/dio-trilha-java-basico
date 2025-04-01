681870060e0c5f91c24af113e37b8ec78ee23087
Rodrigo Yakuwa <rodrigoyakuwa@gmail.com>
2025-04-01 13:07:49 -0300

    Desafio DIO Simulando uma conta bancária

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        //Exibir as mensagens para o nosso usuário.
+
+        System.out.println("Digite o número da agência: ");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Digite sua conta com o dígito: ");
+        String conta = scanner.next();
+ 
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite o seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+
+
+        //obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem final
+
+        System.out.println("Olá " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
